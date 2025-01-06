f48a8c42b6b1349f934d7e9d5127daaa60f2ad91
jylim-dev <jylim.dev@gmail.com>
2025-01-05 21:05:10 -0800

    fix. leaderboard css

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -30,24 +30,28 @@ const Leaderboard = () => {
   }, []);
   return (
     <Container>
-      <Typography variant="h1" sx={{ color: "gold" }}>
+      <Typography variant="h1" sx={{ color: "gold", fontSize: "3rem" }}>
         Leaderboard!
       </Typography>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead sx={{ backgroundColor: "lightgray" }}>
             <TableRow>
+              <TableCell>No.</TableCell>
               <TableCell>User Name</TableCell>
               <TableCell>Score</TableCell>
               <TableCell>Date completed</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {data.map((row) => (
+            {data.map((row, idx) => (
               <TableRow
                 key={row.name}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
+                <TableCell component="th" scope="row">
+                  {idx + 1}
+                </TableCell>
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
