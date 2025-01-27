56c963d9faaf3013b2b3a7b332000034eb1e794c
jylim-dev <jylim.dev@gmail.com>
2025-01-26 21:39:26 -0800

    remove unnecessary functions

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -12,18 +12,6 @@ export function getItem(key) {
   }
 }
 
-export function removeItem(key) {
-  localStorage.removeItem(key);
-}
-
-export function clearStorage() {
-  localStorage.clear();
-}
-
 export function hasKey(key) {
   return localStorage.getItem(key) !== null;
-}
-
-export function getAllKeys() {
-  return Object.keys(localStorage);
 }
