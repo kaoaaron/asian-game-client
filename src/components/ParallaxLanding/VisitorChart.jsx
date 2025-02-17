88de7759fc96d80a53d22afb87d38f3a495eb38d
jylim-dev <jylim.dev@gmail.com>
2025-02-16 22:03:08 -0800

    cache stats for 1 day

--- src/components/ParallaxLanding/VisitorChart.jsx
+++ src/components/ParallaxLanding/VisitorChart.jsx
@@ -1,83 +1,114 @@
 import Chart from "chart.js/auto";
 import { useEffect, useRef } from "react";
 import { fetchVisitorCountHistory } from "../../api";
+import { hasKey, setItem, getItem } from "../../utils/localStorage";
+
+const VISITOR_COUNT_KEY = "visitorCount";
+const msCacheExpiryTime = 86400000;
 
 const VisitorChart = () => {
   const chartInstanceRef = useRef(null);
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const getData = async () => {
-      const result = await fetchVisitorCountHistory();
-      if (result) {
-        result.pop();
+    let result = [];
 
-        let labels = [];
-        for (let i = 0; i < result.length; ++i) {
-          if (i % 10 === 0) labels.push(result[i].date.toString());
-          else labels.push("");
+    const initializeChart = async () => {
+      if (hasKey(VISITOR_COUNT_KEY)) {
+        const { count, expires } = getItem(VISITOR_COUNT_KEY);
+        if (expires > Date.now()) {
+          result = count;
+        } else {
+          result = await fetchVisitorCountHistory();
+          if (result) {
+            result.pop();
+          }
+
+          setItem(VISITOR_COUNT_KEY, {
+            ...(getItem(VISITOR_COUNT_KEY) ?? null),
+            count: result,
+            expires: Date.now() + msCacheExpiryTime,
+          });
         }
-        const data = {
-          labels: labels,
-          datasets: [
-            {
-              label: "Visitors",
-              data: [...result.map((x) => x.count)],
-              borderColor: "#ff8c00",
-              tension: 0.4,
-              pointStyle: false,
-              fill: true,
-            },
-          ],
-        };
+      } else {
+        result = await fetchVisitorCountHistory();
+        if (result) {
+          result.pop();
+        }
 
-        const config = {
-          type: "line",
-          data: data,
-          options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            plugins: {
-              title: {
-                display: true,
-                text: "Unique visitors per day",
-              },
-              legend: {
-                display: false,
-              },
+        setItem(VISITOR_COUNT_KEY, {
+          ...(getItem(VISITOR_COUNT_KEY) ?? null),
+          count: result,
+          expires: Date.now() + msCacheExpiryTime,
+        });
+      }
+
+      let labels = [];
+      for (let i = 0; i < result.length; ++i) {
+        if (i % 10 === 0) labels.push(result[i].date.toString());
+        else labels.push("");
+      }
+
+      const data = {
+        labels: labels,
+        datasets: [
+          {
+            label: "Visitors",
+            data: [...result.map((x) => x.count)],
+            borderColor: "#ff8c00",
+            tension: 0.4,
+            pointStyle: false,
+            fill: true,
+          },
+        ],
+      };
+
+      const config = {
+        type: "line",
+        data: data,
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            title: {
+              display: true,
+              text: "Unique visitors per day",
             },
-            interaction: {
-              intersect: false,
-            },
-            scales: {
-              x: {
-                display: true,
-                title: {
-                  display: true,
-                },
-              },
-              y: {
-                display: true,
-                title: {
-                  display: true,
-                  text: "number of people",
-                },
-                suggestedMin: 0,
-                suggestedMax: 100,
-              },
+            legend: {
+              display: false,
             },
           },
-        };
+          interaction: {
+            intersect: false,
+          },
+          scales: {
+            x: {
+              display: true,
+              title: {
+                display: true,
+              },
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: "number of people",
+              },
+              suggestedMin: 0,
+              suggestedMax: 100,
+            },
+          },
+        },
+      };
 
-        if (chartInstanceRef.current) {
-          chartInstanceRef.current.destroy();
-        }
-
-        chartInstanceRef.current = new Chart(chartRef.current, config);
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
       }
+
+      chartInstanceRef.current = new Chart(chartRef.current, config);
     };
 
-    getData();
+    initializeChart();
 
     return () => {
       if (chartInstanceRef.current) {
